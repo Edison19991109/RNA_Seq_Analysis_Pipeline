4d41ac66f18ade604ca4d559a76cb36b19a4791a
Edison <jianning.18@intl.zju.edu.cn>
2020-06-26 17:06:45 +0800

    Add the alignment, format transformation and assignment

---
+++ codes/Bowtie2_SAMtools_Htseq_cout.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+	sample_header="SRR18140"
+	sample_id=`seq 77 86`
+	reference_directory=
+	gtf_directory=
+	
+	for i in ${sample_id};
+	do
+	bowtie2 -x ${reference_directory} -U ./3_CUTADAPT/${sample_header}${i}_trimmed.fastq  -S ./4_BOWTIE2/${sample_header}${i}_trimmed.sam >./log/${sample_header}${i}_BOWTIE2_standard_output 2>./log/${sample_header}${i}_BOWTIE2_log
+	done;
+
+	for i in ${sample_id};
+	do
+	samtools view -bSF 4 -o ./5_SAMTOOLS/${sample_header}${i}_trimmed.bam ./4_BOWTIE2/${sample_header}${i}_trimmed.sam >./log/${sample_header}${i}_SAMTOOLS_1_standard_output 2>./log/${sample_header}${i}_SAMTOOLS_1_log
+	samtools sort -n -o ./5_SAMTOOLS/${sample_header}${i}_trimmed_srt.bam ./5_SAMTOOLS/${sample_header}${i}_trimmed.bam >./log/${sample_header}${i}_SAMTOOLS_2_standard_output 2>./log/${sample_header}${i}_SAMTOOLS_2_log
+	done;
+
+	for i in ${sample_id};
+	do
+	htseq-count -f bam -r name -s no ./5_SAMTOOLS/${sample_header}${i}_trimmed_srt.bam ${gtf_directory} >./6_HTSEQCOUNT/${sample_header}${i}_Count.txt 2>./log/${sample_header}${i}_HTSEQCOUNT_log
+	done; 
