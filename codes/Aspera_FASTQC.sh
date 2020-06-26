49affa5eda92fb1fa2e1bc6ce0618ed9269daf41
Edison <jianning.18@intl.zju.edu.cn>
2020-06-26 13:27:53 +0800

    Add the downloading and quality control scripts

---
+++ codes/Aspera_FASTQC.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+	sample_header="SRR18140"
+	sample_id=`seq 77 86`
+	
+	for i in ${sample_id};
+	do
+	var=`expr ${i} % 10`
+	ascp -P 33001 -i ~/asperaweb_id_dsa.openssh  era-fasp@fasp.sra.ebi.ac.uk:/vol1/fastq/SRR181/00${var}/${sample_header}${i}/${sample_header}${i}.fastq.gz ./1_RAW >./log/${sample_header}${i}_ASCP_standard_output 2>./log/${sample_header}${i}_RAW_log
+	gunzip ./1_RAW/${sample_header}${i}.fastq.gz 2>./log/${sample_header}${i}_gunzip_log
+	done;
+
+	for i in ${sample_id};
+	do
+	fastqc -o ./2_FASTQC ./1_RAW/${sample_header}${i}.fastq >./log/${sample_header}${i}_FASTQC_standard_output 2>./log/${sample_header}${i}_FASTQC_log;
+	done;
